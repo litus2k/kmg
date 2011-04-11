0785dea7b2f53b6f4f3946731eb5ece934bba057
litus2k@gmail.com <litus2k@gmail.com@85913e78-4305-11de-b77d-cbd14124f061>
2011-04-11 21:52:22 +0000

    completar y corregir

--- trunk/upload.php
+++ trunk/upload.php
@@ -6,7 +6,7 @@ define('ERROR_NO_IMAGE_TYPE_UPLOADED'	, 'ERROR_NO_IMAGE_TYPE_UPLOADED');
 define('ERROR_NO_IMAGE_MIME_FILE'		, 'ERROR_NO_IMAGE_MIME_FILE');
 
 define('ERROR_FORMATO_PROHIBIDO'		, 'ERROR_FORMATO_PROHIBIDO');
-define('ERROR_NO_FORMATO_IMAGEN_VALIDO'	, 'ERROR_NO_FORMATO_IMAGEN_VALIDO');
+define('ERROR_FORMATO_IMAGEN_NO_VALIDO'	, 'ERROR_FORMATO_IMAGEN_NO_VALIDO');
 
 
 $raiz = realpath('./') . '/';	//carpeta script
@@ -16,33 +16,45 @@ define('CARPETA_UPLOADS' 	, $raiz . 'ups/');
 define('CARPETA_IMAGENES'	, CARPETA_UPLOADS . 'img/');
 define('SUBCARPETA_MINIS'	, '_min/');
 
-define('FORMATOS_PROHIBIDOS'	, array('php', 'php3', 'php4', 'phtml','exe'));
-define('FORMATOS_IMAGENES'		, array('jpg', 'png', 'gif', 'jpeg'));
+define('FORMATOS_PROHIBIDOS'	, 'php,php3,php4,phtml,exe');
+define('FORMATOS_IMAGENES'		, 'jpg,png,gif,jpeg');
 
 
-if (isset($_FILES['archivo'])) 
-	guardar_archivo_subido($_FILES['archivo']);
+define('CALIDAD_COMPRESION', 3);
 
-	
-if (isset($_FILES['imagen'])) 
-	guardar_imagen_subida($_FILES['imagen']);
+if (archivo_subido('archivo')) 
+	guardar_archivo_subido('archivo');
+
+if (archivo_subido('imagen')) 
+	guardar_imagen_subida('imagen');
 	
 
 //
 
-function guardar_imagen_subida($as, $ruta_destino = '', $opciones = array)
+function archivo_subido($nombre_upload)
 {
+	return isset($_FILES[$nombre_upload]) && !empty($_FILES[$nombre_upload]['tmp_name']) ;
+}
+
+function guardar_imagen_subida($nombre_upload, $ruta_destino = '', $opciones = array())
+{
+	$as = $_FILES[$nombre_upload];
+	
 	comprobar_upload($as);
 	
-	if(! eregi('image/', $as['type']))
+	//if(! eregi('image/', $as['type']))
+	
+	if(! stristr( $as['type'], 'image/' ))
 		die(ERROR_NO_IMAGE_TYPE_UPLOADED);
 	
-	$info = getImageSize($as['tmp_name']);
-	if(! eregi('image/', $info['mime']))
+	$info = datos_archivo_imagen($as['tmp_name']);
+	
+	//if(! eregi('image/', $info['mime']))
+	if(! stristr( $info['mime'], 'image/' ))
 		die(ERROR_NO_IMAGE_MIME_FILE);	
 	
-	if (! existe_extension($as['name'], FORMATOS_IMAGENES))
-		die(ERROR_NO_FORMATO_IMAGEN_VALIDO);
+	if (! existe_extension($as['name'], explode(',',FORMATOS_IMAGENES)))
+		die(ERROR_FORMATO_IMAGEN_NO_VALIDO);
 	
 	$ruta_origen = $as['tmp_name'];
 
@@ -52,14 +64,36 @@ function guardar_imagen_subida($as, $ruta_destino = '', $opciones = array)
 	//
 	
 	$imagen = imageCreateFromString(file_get_contents($ruta_origen));
+
+	$_guardar_imagen = funcion_guardar_imagen(extension($ruta_destino));
 	
-	$ruta_imagen_final = $ruta_destino;
+	$_guardar_imagen($imagen, $ruta_destino); //, CALIDAD_COMPRESION);
 	
-	return $ruta_imagen_final;
+	//
+	
+	return $ruta_destino;
 }
 	
-function guardar_archivo_subido($as, $ruta_destino = '')
+function funcion_guardar_imagen($extension)
 {
+	switch(strtolower($extension))
+	{
+		case 'jpg':
+		case 'jpeg': return 'imagejpeg'; break;
+		
+		default: return 'image'.$extension;
+	}
+}
+	
+function datos_archivo_imagen($ruta)
+{
+	return getImageSize($ruta);
+}
+	
+function guardar_archivo_subido($nombre_upload, $ruta_destino = '')
+{
+	$as = $_FILES[$nombre_upload];
+	
 	comprobar_upload($as);
 	
 	if (empty($ruta_destino))
@@ -73,10 +107,14 @@ function guardar_archivo_subido($as, $ruta_destino = '')
 	return $ruta_destino;
 }
 
+function extension($ruta_archivo)
+{
+	return end(explode('.', strtolower($ruta_archivo)));
+}
 
 function existe_extension($nombre_archivo, $lista_extensiones)
 {
-	return in_array(end(explode('.', strtolower($nombre_archivo))), $lista_extensiones);
+	return in_array(extension($nombre_archivo), $lista_extensiones);
 }
 
 function comprobar_upload($as)
@@ -87,7 +125,7 @@ function comprobar_upload($as)
 	if (! is_uploaded_file($as['tmp_name']))
 		die(ERROR_NOT_A_UPLOADED_FILE . ' ' . $as['tmp_name']);	
 		
-	if (existe_extension($as['name'], FORMATOS_PROHIBIDOS)
+	if (existe_extension($as['name'], explode(',',FORMATOS_PROHIBIDOS)))
 		die(ERROR_FORMATO_PROHIBIDO);
 		
 	//if (file_size($as['tmp_name']) > MAX_PESO_UPLOAD)
@@ -113,6 +151,7 @@ function texto_error_upload($codigo_error)
 <body>
 <form enctype=multipart/form-data  method=POST>
 <input name=archivo type=file >
+<input name=imagen type=file >
 <input type=submit >
 </form>
 </body>
